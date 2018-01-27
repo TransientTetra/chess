eb7faa68b7317ca61376d054dad77583d6f0a635
mikspe <msperkowski@airmail.cc>
2018-01-27 14:51:49 +0100

    added sdl window

--- headers.h
+++ headers.h
@@ -1,6 +1,13 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "struct.h"
 
+#define SCREEN_WIDTH 300
+#define SCREEN_HEIGHT 300
+#define BLUE SDL_MapRGB(screen->format, 0x00, 0x00, 0xFF)
+#define WHITE SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF)
+#define RED SDL_MapRGB(screen->format, 0xFF, 0x00, 0x00)
+#define GREEN SDL_MapRGB(screen->format, 0x00, 0xFF, 0x00)
--- main.c
+++ main.c
@@ -4,7 +4,42 @@
 
 int main(int argc, char const *argv[])
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window *window = NULL;
+	SDL_Surface *screen = NULL;
+
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		fprintf(stderr, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	window = SDL_CreateWindow("Chess", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == NULL)
+	{
+		fprintf(stderr, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	screen = SDL_GetWindowSurface(window);
+	SDL_ShowCursor(SDL_DISABLE);
+
+	for (int i = 0; i < 100; ++i)
+	{
+		if (i % 2)
+		{
+			SDL_FillRect(screen, NULL, GREEN);
+		}
+		else
+		{
+			SDL_FillRect(screen, NULL, RED);
+		}
+		SDL_UpdateWindowSurface(window);
+		SDL_Delay(100);
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_FreeSurface(screen);
 	SDL_Quit();
 
 	return 0;
